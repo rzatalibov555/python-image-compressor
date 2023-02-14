dfabfa59183c98103dca41d4e828a8aaca427715
rzatalibov555 <sethubpro@gmail.com>
2023-02-14 19:06:27 +0400

    remove limit

--- venv/index.py
+++ venv/index.py
@@ -1,7 +1,11 @@
-from PIL import Image
+from PIL import Image, ImageFile
 import glob
+import PIL.Image
+PIL.Image.MAX_IMAGE_PIXELS = 933120000
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-types = ("*.png","*.jpg")
+
+types = ("*.png","*.jpg","*.jpeg","*.gif","*jfif")
 files_grabbed = []
 
 for files in types:
@@ -12,7 +16,7 @@ print(f"Finded files: {len(files_grabbed)}")
 for i in files_grabbed:
     img = Image.open(i)
    
-    if img.height > 300 or img.width > 300:
+    if img.height > 1 or img.width > 1:
         output_size = (1600, 1400)
         img.thumbnail(output_size)
         img.save(f"output/{i}")
